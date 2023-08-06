5df7adab0395a4beaea9279f07ea908e1dbb9ed4
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-08-06 15:42:56 +0300

    sign in and sign up also finisshed

--- App.tsx
+++ App.tsx
@@ -2,14 +2,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import List from './app/screens/List';
 import Details from './app/screens/Details';
+import Login from "./app/screens/Login";
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
+  {/*
+    import {useState, useEffect} from "react";
+    import { onAuthStateChanged } from 'firebase/auth';
+    const [authenticated, setAuthenticated] = useState(false);
+    useEffect(() => {
+      onAuthStateChanged
+    }, [])
+  */}
+
+  
   return (
+
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name="My Todos" component={List} />
         <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
 
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,47 +1,88 @@
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
-import React, {useState} from 'react'
+import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const signup = () => {
+    const auth = getAuth();
+
+    const signup = async () => {
+        await createUserWithEmailAndPassword(auth, email, password);
+        console.log("check your email")
     }
 
-    const signin = () => {
-      
+    const signin = async () => {
+        const user = await signInWithEmailAndPassword(auth, email, password);
+        console.log(`user: ${user}`);
     }
   return (
     <View>
         <View style={styles.todoContainer}>
-          <TextInput 
-            placeholder='Enter Email'
-            placeholderTextColor='#999'
-            style={styles.inputArea}
-            onChangeText={(text:string) => setEmail(text)}
-            value={email} />
-          <Button onPress={signup} title="Sign Up"
-          disabled={ todo === ""}/>
+            <View>
+                <TextInput 
+                    placeholder='Enter Email'
+                    placeholderTextColor='#999'
+                    style={styles.inputArea}
+                    onChangeText={(text:string) => setEmail(text)}
+                    value={email} />
+                <TextInput 
+                    placeholder='Enter Password'
+                    placeholderTextColor='#999'
+                    textContentType='password'
+                    style={styles.inputArea}
+                    onChangeText={(text:string) => setPassword(text)}
+                    value={password} />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={signup} style={styles.buttonStyle} disabled={email === "" && password === ""}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={signin} style={styles.buttonStyle} disabled={email === "" && password === ""}>
+                    <Text style={styles.buttonText}>Sign In</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
-    mainContainer: {
-        marginHorizontal: 20,
-      },
       todoContainer: {
         marginVertical: 20,
-        flexDirection: "row",
+        flexDirection: "column",
         alignItems: "center",
+        
       },
       inputArea: {
-        flex: 1,
+        minWidth: 250,
         height: 40,
         borderWidth: 1,
         borderRadius: 4,
         padding: 20,
+        marginTop: 10,
         backgroundColor: "#fff",
+      },
+      buttonContainer: {
+        display: "flex",
+        flexDirection: "row",
+        marginTop: 10,
+        justifyContent: "space-around",
+      },
+    
+      // Custom button styles
+      buttonStyle: {
+        minWidth: 100,
+        backgroundColor: "#007bff",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 4,
+        flex: 1, // Add flex property to make the buttons share the available space
+        marginHorizontal: 5, // Add horizontal space between the buttons
+      },
+      buttonText: {
+        color: "#fff",
+        textAlign: "center",
       },
 })
 export default Login
